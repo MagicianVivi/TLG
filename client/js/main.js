52d8246864a00c226136bbde6b3f8af89d51eea3
Vivien Alger <vivien.alger@gmail.com>
2013-11-02 13:21:51 +0100

    Updating repository structure

--- client/js/main.js
+++ client/js/main.js
@@ -1,4 +1,4 @@
-require(['domReady!', './api'], function (domReady, api) {
+require(['lib/requirejs/domReady!', 'modules/api'], function (domReady, api) {
     'use strict';
     document.getElementById('submit_query').onclick = api.call_search_api;
 });
